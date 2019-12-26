f9bd8011d21750d0161422b4f6c478daf9f476cf
“CPyeah <chengpengyeah@126.com>
2019-12-26 14:20:51 +0800

    库/表的管理 练习

---
+++ my_exercise/2019年12月26日 - 表/库的管理练习.sql
@@ -0,0 +1,25 @@
+#1.	创建表dept1
+NAME	NULL?	TYPE
+id		INT(7)
+NAME		VARCHAR(25)
+
+#2.	将表departments中的数据插入新表dept2中
+
+#3.	创建表emp5
+NAME	NULL?	TYPE
+id		INT(7)
+First_name	VARCHAR (25)
+Last_name	VARCHAR(25)
+Dept_id		INT(7)
+
+#4.	将列Last_name的长度增加到50
+
+#5.	根据表employees创建employees2
+
+#6.	删除表emp5
+
+#7.	将表employees2重命名为emp5
+
+#8.在表dept和emp5中添加新列test_column，并检查所作的操作
+
+#9.直接删除表emp5中的列 dept_id
