b8f9818f8072216c113e7d8d56601774728e21bd
“CPyeah <chengpengyeah@126.com>
2020-01-16 10:38:01 +0800

    函数

---
+++ my_exercise/2020年1月16日 - 函数.sql
@@ -0,0 +1,102 @@
+#函数
+/*
+含义：一组预先编译好的SQL语句的集合，理解成批处理语句
+1、提高代码的重用性
+2、简化操作
+3、减少了编译次数并且减少了和数据库服务器的连接次数，提高了效率
+
+区别：
+
+存储过程：可以有0个返回，也可以有多个返回，适合做批量插入、批量更新(增删改)
+函数：有且仅有1 个返回，适合做处理数据后返回一个结果(查询)
+
+*/
+
+#一、创建语法
+CREATE FUNCTION 函数名(参数列表) RETURNS 返回类型
+BEGIN
+	函数体
+END
+/*
+
+注意：
+1.参数列表 包含两部分：
+参数名 参数类型
+
+2.函数体：肯定会有return语句，如果没有会报错
+如果return语句没有放在函数体的最后也不报错，但不建议
+
+return 值;
+3.函数体中仅有一句话，则可以省略begin end
+4.使用 delimiter语句设置结束标记
+
+*/
+
+#二、调用语法
+SELECT 函数名(参数列表)
+
+
+#------------------------------案例演示----------------------------
+delimiter $
+#1.无参有返回
+#案例：返回公司的员工个数
+CREATE FUNCTION get_emp_count() RETURNS INT
+BEGIN
+
+	DECLARE c INT DEFAULT 0;#定义局部变量
+	SELECT COUNT(*) INTO c#赋值
+	FROM employees;
+	RETURN c;
+
+END $
+
+SELECT get_emp_count()$
+
+
+#2.有参有返回
+#案例1：根据员工名，返回它的工资
+delimiter $
+CREATE FUNCTION get_salary_by_empName(empName VARCHAR(20)) RETURNS DOUBLE
+BEGIN
+	SET @sal=0;#定义用户变量
+	SELECT salary INTO @sal   #赋值
+	FROM employees
+	WHERE last_name = empName;
+
+	RETURN @sal;
+END $
+
+SELECT get_salary_by_empName('Kochhar');
+
+#案例2：根据部门名，返回该部门的平均工资
+delimiter $
+CREATE FUNCTION get_salAvg_by_deptName(deptName VARCHAR(20)) RETURNS DOUBLE
+BEGIN
+	DECLARE sal DOUBLE ;
+	SELECT AVG(salary) INTO sal
+	FROM employees e
+	JOIN departments d ON e.department_id = d.department_id
+	WHERE d.department_name=deptName;
+	RETURN sal;
+END $
+
+SELECT get_salAvg_by_deptName('IT');
+
+#三、查看函数
+
+SHOW CREATE FUNCTION get_salAvg_by_deptName;
+
+#四、删除函数
+DROP FUNCTION get_salAvg_by_deptName;
+
+#案例
+#一、创建函数，实现传入两个float，返回二者之和
+delimiter $
+CREATE FUNCTION test_add(num1 FLOAT,num2 FLOAT) RETURNS FLOAT
+BEGIN
+	DECLARE SUM FLOAT DEFAULT 0;
+	SET SUM=num1+num2;
+	RETURN SUM;
+END $
+
+SELECT test_add(1,2) sum;
